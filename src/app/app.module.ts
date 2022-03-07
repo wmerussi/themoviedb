43a7d0e28b8e48869bd91835265cd07ad8c1a4cd
William Merussi <wmerussi@gmail.com>
2022-03-07 15:45:43 -0300

    creating components folder and moving home to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
