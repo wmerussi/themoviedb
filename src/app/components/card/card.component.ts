9bf323ccb91e78acd4809e5590956192371e87a8
William Merussi <wmerussi@gmail.com>
2022-03-10 05:54:23 -0300

    fixed image not found

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,16 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  src: string = '';
   @Input() title: string = '';
   @Input() date: string = '';
   @Input() rating: number = 0;
-
-  @Input() set imageSrc(value: string) {
-    this.src = value;
-  };
-
-  loadAltImage(): void {
-    this.src = 'assets/images/cover.jpg';
-  }
+  @Input() imageSrc: string = '';
 }
